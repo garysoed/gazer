91a8e61f287de930de5e3bc54ecfd98b9dded181
Gary Soedarsono <garysoed@gmail.com>
2015-07-06 18:35:00 -0700

    use loki to keep track of card db, split play area into 2, changed references to names to be lower case, added logic to reset life when dropped into play area

--- extractcard.js
+++ extractcard.js
@@ -5,15 +5,15 @@ var through = require('through2');
 
 module.exports = function() {
   return through.obj(function(file, enc, cb) {
-    var elements = {};
+    var cards = [];
     if (file.isBuffer()) {
       var lines = String(file.contents).split('\n');
-      for (var i = 1; i < lines.length; i++) {
+      for (var i = 2; i < lines.length; i++) {
         var segments = lines[i].split('\t');
         var card = {
           'name': segments[0],
-          'type': segments[2],
-          'element': segments[1],
+          'type': segments[2].toLowerCase(),
+          'element': segments[1].toLowerCase(),
           'count': segments[3],
           'description': segments[7],
           'cost': segments[8],
@@ -32,18 +32,20 @@ module.exports = function() {
           card['armor'] = segments[6];
         }
 
-        if (!elements[card.element]) {
-          elements[card.element] = [];
-        }
-        elements[card.element].push(card);
+        cards.push(card);
       }
 
       var content =
           '<base href="..">'
         + '<link rel="import" href="src/dijs.html">'
+        + '<script src="bower_components/lokijs/src/lokijs.js"></script>'
         + '<script>'
           + 'DI.prefix("gz").bind("data", {}, function() { '
-            + 'return ' + JSON.stringify(elements, null, 2) + ';'
+            + 'var raw = ' + JSON.stringify(cards, null, 2) + ';'
+            + 'var db = new loki("gazer");'
+            + 'var cards = db.addCollection("cards");'
+            + 'raw.forEach(function(data) { cards.insert(data); });'
+            + 'return cards;'
           + '});'
         + '</script>'
       this.push(new gutil.File({
